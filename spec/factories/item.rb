740c163a1bc651dcef5f4edb06ec9b5ab87a42a6
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-29 20:57:05 -0400

    Update: Item factory bot model & test instance

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :item do
+    merchant
     name { Faker::Device.model_name }
     description { Faker::Device.manufacturer }
     unit_price { Faker::Number.decimal(l_digits: 2) }
--- spec/requests/api/v1/merchants/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants/merchants_request_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe "Get All Merchants" do 
  it "sends a list of merchants" do 
     create_list(:merchant, 3)
-    create_list(:item, 3)
+    create_list(:item, 3, merchant_id: Merchant.first.id)
 
     get "/api/v1/merchants"
 
     merchants = JSON.parse(response.body, symbolize_names: true)  
-      
     expect(response).to be_successful 
     expect(merchants.count). to eq(3)
 
